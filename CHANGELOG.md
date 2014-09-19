bfc5e586932b51a35404aa20f38508c5d9542e8f
Frank Slaughter <fgs@tower3.com>
2014-09-19 10:28:47 -0700

    v0.0.3: * export beanstalkt exceptions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## v0.0.3:
+* export beanstalkt exceptions
+
 ## v0.0.2:
 * require beanstalkt version 0.6.0
 
--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,7 @@
 from work_queue import TTWorkQueue
+
+from beanstalkt import UnexpectedResponse
+from beanstalkt import CommandFailed
+from beanstalkt import Buried
+from beanstalkt import DeadlineSoon
+from beanstalkt import TimedOut
